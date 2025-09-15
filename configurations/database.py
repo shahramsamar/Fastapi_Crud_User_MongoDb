a37fc9b7276a0126916efca28fe727bab2df8293
shahram <shahramsamar2010@gmail.com>
2025-09-15 23:56:59 +0330

    Create database.py

---
+++ configurations/database.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+
+uri = "mongodb://localhost:27017/?directConnection=true"
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment.You successfilly connected to MongoDb!!!!!")
+
+except Exception as e:
+    print(e)
+
+db = client.users
+users_table = db['user']    
+    
